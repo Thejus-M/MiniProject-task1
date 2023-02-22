dc6184e447b1df426171085f5dcca38ba2ce0b1c
amal <amalej1234@gmail.com>
2023-02-22 16:51:34 +0530

    feat: signup,login,home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name='home/index.html'
+
+class LoginInterfaceView(LoginView):
+    template_name='home/form.html'
+#     model
+
+class SignupView(CreateView):
+    model = User
+    success_url='/'
+
+ 
+    fields = ['username','first_name','last_name','email','password']
